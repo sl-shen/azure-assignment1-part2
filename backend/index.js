a300858cfec38f1e4d86c2dd6fe643e5cd9bd55d
yukinolov <slsteven@bu.edu>
2026-02-02 15:22:27 -0500

    fixed frontend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const port = process.env.PORT ǁ 8080
+const port = process.env.PORT || 8080
 const app = express()
 app.use(express.json())
 app.listen(port, () => console.log('Sample app is listening on port ${port}!'))
 
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('postgres://yukinolov%40assignment1-part2:Ls20021227!@assignment1-part2.postgres.database.azure.com:5432/cnainventory)
+const sequelize = new Sequelize('postgres://yukinolov:Ls20021227@assignment1-part2.postgres.database.azure.com:5432/cnainventory')
 sequelize
 .authenticate()
 .then(() => {
